7548c6e9fcbddec66108004c2c8e20ea703e6cb4
Marcus Beranek <marcus@beranek.de>
2023-03-29 22:02:02 +0200

    Adjusted tests for WriteThrough.

--- CachingDemo/CachingDemo.Tests/WriteThroughStrategyTests.cs
+++ CachingDemo/CachingDemo.Tests/WriteThroughStrategyTests.cs
@@ -1,87 +1,119 @@
 ﻿namespace CachingDemo.Tests
 {
-    /*
     [TestClass]
     public class WriteThroughStrategyTests
     {
         private MockRepository mockRepository;
+        private Mock<ObjectCache> cacheMock;
+        private Mock<IRepository<string, string>> repoMock;
 
         [TestInitialize]
         public void TestInitialize()
         {
-            this.mockRepository = new MockRepository(MockBehavior.Strict);
+            mockRepository = new MockRepository(MockBehavior.Strict);
+            cacheMock = mockRepository.Create<ObjectCache>();
+            repoMock = mockRepository.Create<IRepository<string, string>>();
         }
 
-        private WriteThroughStrategy CreateWriteThroughStrategy()
+        private WriteThroughStrategy<string, string> CreateWriteThroughStrategy()
         {
-            return new WriteThroughStrategy();
+            return new WriteThroughStrategy<string, string>();
         }
 
         [TestMethod]
-        public void Create_StateUnderTest_ExpectedBehavior()
+        public void Create_Through_Cache_And_Repo()
         {
             // Arrange
-            var writeThroughStrategy = this.CreateWriteThroughStrategy();
-            T item = null;
-            ObjectCache cache = null;
-            CacheItemPolicy policy = null;
-            IRepository repo = null;
+            var writeThroughStrategy = CreateWriteThroughStrategy();
+            string item = "data";
+            CacheItemPolicy policy = new CacheItemPolicy();
+
+            repoMock.Reset();
+            cacheMock.Reset();
+
+            // Repo.Create must be called once.
+            repoMock
+                .Setup(r => r.Create(It.IsAny<string>()))
+                .Returns("key from repo");
+
+            // Cache.Add must also be called once to store the data.
+            cacheMock
+                .Setup(c => c.Add(It.IsAny<CacheItem>(), It.IsAny<CacheItemPolicy>()))
+                .Returns(true);
 
             // Act
             var result = writeThroughStrategy.Create(
                 item,
-                cache,
+                cacheMock.Object,
                 policy,
-                repo);
+                repoMock.Object);
 
             // Assert
-            Assert.Fail();
             this.mockRepository.VerifyAll();
         }
 
         [TestMethod]
-        public void Delete_StateUnderTest_ExpectedBehavior()
+        public void Delete_Through_Cache_And_Repo()
         {
             // Arrange
-            var writeThroughStrategy = this.CreateWriteThroughStrategy();
-            TKey key = default(TKey);
-            ObjectCache cache = null;
-            IRepository repo = null;
+            var writeThroughStrategy = CreateWriteThroughStrategy();
+            string key = "key";
+            CacheItemPolicy policy = new CacheItemPolicy();
+
+            repoMock.Reset();
+            cacheMock.Reset();
+
+            // Repo.Create must be called once.
+            repoMock
+                .Setup(r => r.Delete(It.IsAny<string>()))
+                .Returns(true);
+
+            // Cache.Add must also be called once to store the data.
+            cacheMock
+                .Setup(c => c.Remove(It.IsAny<string>(), null))
+                .Returns(true);
 
             // Act
             var result = writeThroughStrategy.Delete(
                 key,
-                cache,
-                repo);
+                cacheMock.Object,
+                repoMock.Object);
 
             // Assert
-            Assert.Fail();
-            this.mockRepository.VerifyAll();
+            mockRepository.VerifyAll();
         }
 
         [TestMethod]
-        public void Write_StateUnderTest_ExpectedBehavior()
+        public void Write_Through_Cache_And_Repo()
         {
             // Arrange
-            var writeThroughStrategy = this.CreateWriteThroughStrategy();
-            TKey key = default(TKey);
-            T item = null;
-            ObjectCache cache = null;
-            CacheItemPolicy policy = null;
-            IRepository repo = null;
+            var writeThroughStrategy = CreateWriteThroughStrategy();
+            string key = "key";
+            string item = "data";
+            CacheItemPolicy policy = new CacheItemPolicy();
+
+            repoMock.Reset();
+            cacheMock.Reset();
+
+            // Repo.Create must be called once.
+            repoMock
+                .Setup(r => r.Update(It.IsAny<string>(), It.IsAny<string>()))
+                .Returns(true);
+
+            // Cache.Add must also be called once to store the data.
+            cacheMock
+                .Setup(c => c.Set(It.IsAny<CacheItem>(), It.IsAny<CacheItemPolicy>()));
 
             // Act
             var result = writeThroughStrategy.Write(
                 key,
                 item,
-                cache,
+                cacheMock.Object,
                 policy,
-                repo);
+                repoMock.Object);
 
             // Assert
-            Assert.Fail();
-            this.mockRepository.VerifyAll();
+            mockRepository.VerifyAll();
         }
     }
-    */
 }
